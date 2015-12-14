8c97e5456b154bc47a2d41110d2aa87a3b9ce496
andryyy <andre.peters@debinux.de>
2015-12-14 18:29:01 +0100

    SOGo mailcow png

--- webserver/htdocs/mail/index.php
+++ webserver/htdocs/mail/index.php
@@ -60,9 +60,9 @@ $_SESSION['return_to'] = basename($_SERVER['PHP_SELF']);
 	<br />Your credentials will not be transfered until a secure session was initiated.
 	</p>
 	<h4>Microsoft ActiveSync</h4>
-	<p>ActiveSync support is enabled.</p>
+	<p>ActiveSync support is enabled. Use <?=$_SERVER['SERVER_NAME'];?> as server name.</p>
 	<h4>Cal- and CardDAV</h4>
-	<p><a href="user.php" style="text-decoration:underline">Navigate to your personal settings</a> and copy the full path of your desired calendar or address book.</p>
+	<p>Use https://<?=$_SERVER['SERVER_NAME'];?>/SOGo/dav/ as path to DAV services.</p>
 	<h4>Health check (© MXToolBox)</h4>
 	<p>"The Domain Health Check will execute hundreds of domain/email/network performance tests to make sure all of your systems are online and performing optimally. The report will then return results for your domain and highlight critical problem areas for your domain that need to be resolved."</p>
 	<a class="btn btn-default" href="http://mxtoolbox.com/SuperTool.aspx?action=smtp:<?php echo $MYHOSTNAME ?>" target="_blank">Run &raquo;</a>
