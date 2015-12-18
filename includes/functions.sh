15ec3d88255cd5ec70830ff3b8eb42bb837003a1
andryyy <andre.peters@debinux.de>
2015-12-18 23:02:06 +0100

    Fixes to Apache2 site

--- includes/functions.sh
+++ includes/functions.sh
@@ -470,8 +470,9 @@ DatabaseMirror clamav.inode.at" >> /etc/clamav/freshclam.conf
 			rm /etc/apache2/sites-enabled/{mailcow*,000-0-mailcow,000-0-fufix,000-0-mailcow.conf} 2>/dev/null
 			cp webserver/apache2/conf/sites-available/mailcow.conf /etc/apache2/sites-available/
 			ln -s /etc/apache2/sites-available/mailcow.conf /etc/apache2/sites-enabled/000-0-mailcow.conf 2>/dev/null
-			sed -i "s/MAILCOW_HOST/${sys_hostname}/g" /etc/apache2/sites-available/mailcow.conf
-			sed -i "s/MAILCOW_DOMAIN/${sys_domain}/g" /etc/apache2/sites-available/mailcow.conf
+			rm -f /etc/apache/conf-enabled/SOGo.conf
+			sed -i "s/sys_hostname/${sys_hostname}/g" /etc/apache2/sites-available/mailcow.conf
+			sed -i "s/sys_domain/${sys_domain}/g" /etc/apache2/sites-available/mailcow.conf
 			a2enmod rewrite ssl headers cgi proxy proxy_http > /dev/null 2>&1
 			mkdir /var/lib/php5/sessions 2> /dev/null
 			cp -R webserver/htdocs/mail /var/www/
